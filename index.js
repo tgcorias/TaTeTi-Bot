17d4352393820ebe4d942292814448b74f01a3ad
tgcorias <ticianocorias@gmail.com>
2021-08-24 14:13:53 -0300

    Primeras interacciones

---
+++ index.js
@@ -0,0 +1,28 @@
+const { Client, Intents } = require('discord.js');
+const config = require('./config.json');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.login(config.BOT_TOKEN); 
+
+let prefix = config.prefix;
+
+client.on('ready', ()=>{
+    console.log(`On como ${client.user.tag}`);
+    client.user.setStatus('idle');
+})
+
+
+client.on('message',(message)=>{
+
+    console.log(message.content);
+
+    if (message.content == 'hola'){
+        
+            message.reply(`Hola <@${message.author.id}>`);   
+    };
+
+    if (message.content == `${prefix}help`){
+        message.reply("Para más ayuda busca en google! :)");
+    };
+});
+
